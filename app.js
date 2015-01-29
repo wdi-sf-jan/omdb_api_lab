fa8a023dcfec44c40a457ea6cdc132ab364b6b6f
Tim Licata <tim@timothylicata.com>
2015-01-28 19:14:03 -0800

    add bonus solution

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 // interaction. This is jQuery's equivalent of window.onload.
 $(document).ready(function () {
 
+  // Grab a reference to the <div> with an ID of "poster".
+  var $poster = $("#poster");
+
   // $("form") is jQuery's way of doing
   // document.getElementsByTagName("form). Listen to the "submit"
   // event. (This will listen for submit events on ALL the forms in
@@ -10,6 +13,9 @@ $(document).ready(function () {
 
     // Prevent the page from reloading.
     event.preventDefault();
+
+    // Remove any old posters (if any).
+    $poster.empty();
 
     // Can either grab the search term through event.target or this.
     var query = this.searchTerm.value;
@@ -23,7 +29,29 @@ $(document).ready(function () {
         $results.html("No results found.");
       } else {
         data.Search.forEach(function (movie) {
-          var li = $("<li></li>").text(movie.Title);
+          // Create a new list item.
+          var li = $("<li></li>");
+          // Create a new link.
+          var a = $("<a></a>");
+          // Set the link's text to be the title of the movie.
+          a.text(movie.Title);
+          // Set it's `href` attribute so it looks like a link.
+          a.attr("href", "#");
+          // Listen for a click event, in order to load the poster.
+          a.on("click", function (event) {
+            console.log("you clicked " + movie.Title);
+            // Remove old poster (if any).
+            $poster.empty();
+            // Call OMDB for info about a specific movie.
+            $.getJSON("http://www.omdbapi.com/", {i:movie.imdbID}, function (moreData) {
+              console.log(moreData);
+              var img = $("<img>").attr("src", moreData.Poster);
+              $poster.html(img);
+            });
+          });
+          // Add the link to the list item.
+          li.append(a);
+          // Add the list item to the page.
           $results.append(li);
         });
       }
